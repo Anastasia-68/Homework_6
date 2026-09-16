b981574ccae46ecb696ceb77fa958f80bd483413
Anastasia <duval.ana51@gmail.com>
2026-09-17 00:05:48 +0300

    Add digit multiplication homework

---
+++ 6.3.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+while number > 9:
+    result = 1
+
+    while number > 0:
+        digit = number % 10
+        result *= digit
+        number //= 10
+
+    number = result
+
+print(number)
